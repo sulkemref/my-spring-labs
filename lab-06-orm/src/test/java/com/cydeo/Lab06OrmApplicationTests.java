38291ea91700378d45b6f45f75c098ead222cc82
jamaldemir <jamal@cybertekschool.com>
2023-03-29 13:26:11 -0400

    lab-06-orm

---
+++ lab-06-orm/src/test/java/com/cydeo/Lab06OrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab06OrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
